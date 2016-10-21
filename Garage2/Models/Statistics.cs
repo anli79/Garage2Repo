5befe8c59de0fbc130bfdc610daad3a36053606c
anli79 <anders.b.lindgren@gmail.com>
2016-10-21 10:23:36 +0200

    Okej layout för statistik.

--- Garage2/Models/Statistics.cs
+++ Garage2/Models/Statistics.cs
@@ -6,25 +6,25 @@ using System.Web;
 
 namespace Garage2.Models {
     public class Statistics {
-        [Display(Name = "Antal bilar")]
+        [Display(Name = "Antal bilar:")]
         public int NrOfCars { get; set; }
 
-        [Display(Name = "Antal bussar")]
+        [Display(Name = "Antal bussar:")]
         public int NrOfBusses { get; set; }
 
-        [Display(Name = "Antal båtar")]
+        [Display(Name = "Antal båtar:")]
         public int NrOfBoats { get; set; }
 
-        [Display(Name = "Antal flygplan")]
+        [Display(Name = "Antal flygplan:")]
         public int NrOfAirplanes { get; set; }
 
-        [Display(Name = "Antal motorcyklar")]
+        [Display(Name = "Antal motorcyklar:")]
         public int NrOfMotorcycles { get; set; }
 
-        [Display(Name = "Totalt antal hjul i garaget")]
+        [Display(Name = "Totalt antal hjul:")]
         public int NrOfWheels { get; set; }
 
-        [Display(Name = "Totalt pris för alla fordon i garaget")]
+        [Display(Name = "Pris för alla fordon:")]
         public int PriceAllVehicles { get; set; }
     }
 }
