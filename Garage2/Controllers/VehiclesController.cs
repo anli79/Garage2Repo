cc8e8bf9a0662b0a3fc309a29c1e5772517a5906
Thomas <thomas.larnhed@gmail.com>
2016-10-21 13:41:58 +0200

    Friday 13.40

--- Garage2/Controllers/VehiclesController.cs
+++ Garage2/Controllers/VehiclesController.cs
@@ -182,7 +182,6 @@ namespace Garage2.Controllers {
             return RedirectToAction("Receipt", vehicle);
         }
 
-
         // GET: Vehicles/SearchVehicles
         public ActionResult SearchVehicles() {
             return View();
